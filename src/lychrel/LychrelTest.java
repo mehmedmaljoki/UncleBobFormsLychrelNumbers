bfe44955d9e590e7232a4acebc4dcc4068b60cde
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2022-01-18 22:55:20 +0100

    adding some test palindromes

--- src/lychrel/LychrelTest.java
+++ src/lychrel/LychrelTest.java
@@ -1,6 +1,5 @@
 package lychrel;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,7 +8,6 @@ public class LychrelTest {
 
     private final int LIMIT = 1000;
 
-    @Ignore
     @Test
     public void facts() throws Exception {
         convergesAtIteration(1, 0);
@@ -28,19 +26,21 @@ public class LychrelTest {
         isPalindrome(11);
         isPalindrome(121);
         isPalindrome(12321);
+        isPalindrome(1234321);
     }
 
     @Test
     public void nonPalindrome() throws Exception {
         isNotPalindrome(10);
         isNotPalindrome(12331);
+        isNotPalindrome(1243321);
     }
 
     private void isNotPalindrome(int n) {
-        assertFalse(Lychrel.isNotPalindrome(n));
+        assertFalse(Lychrel.isPalindrome(n));
     }
 
     private void isPalindrome(int n) {
-        assertTrue(Lychrel.isNotPalindrome(n));
+        assertTrue(Lychrel.isPalindrome(n));
     }
 }
